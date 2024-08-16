7b48c58fbc764ac470f56110120f6a23af397977
Arsh <iqbalarsh54@gmail.com>
2024-08-16 11:22:12 +0530

    adjusting

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,11 +5,12 @@ export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
       <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+        <div className='w-full items-center justify-between sm:justify-between flex flex-col sm:gap-x-4  gap-y-4 sm:flex-row md:grid-cols-1 '>
+
           <div className='mt-5'>
             <Link
               to='/'
-              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+              className='sm:self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
                 Arsh's
@@ -17,8 +18,12 @@ export default function FooterCom() {
               Blog
             </Link>
           </div>
-          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
-            <div>
+
+          {/* ------------ footer links -------------- */}
+
+          <div className=' sm:w-[60%] w-full grid grid-cols-2 justify-items-center mt-4 sm:grid-cols-3  gap-y-8 sm:gap-6'>
+
+            <div className=''>
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
@@ -37,6 +42,7 @@ export default function FooterCom() {
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
+
             <div>
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
@@ -50,6 +56,7 @@ export default function FooterCom() {
                 <Footer.Link href='#'>Discord</Footer.Link>
               </Footer.LinkGroup>
             </div>
+
             <div>
               <Footer.Title title='Legal' />
               <Footer.LinkGroup col>
@@ -58,15 +65,21 @@ export default function FooterCom() {
               </Footer.LinkGroup>
             </div>
           </div>
+
+
+
         </div>
+
         <Footer.Divider />
-        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+
+
+        <div className='w-full flex flex-col gap-y-2  sm:flex-row items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
             by="Arsh's blog"
             year={new Date().getFullYear()}
           />
-          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+          <div className="flex gap-8 sm:mt-0 mt-4 sm:justify-center justify-between  w-[80%] sm:w-max ">
             <Footer.Icon href='https://www.facebook.com/arsh.iqbal.9066' icon={BsFacebook}/>
             <Footer.Icon href='https://www.linkedin.com/in/arsh-iqbal-338485313' icon={BsLinkedin}/>
             <Footer.Icon href='https://x.com/Arsh_024' icon={BsTwitter}/>
